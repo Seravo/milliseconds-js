da3e492eb5f00beed0ed23d9ce2c87138ef79b60
Antti Kuosmanen <antti@seravo.fi>
2015-07-03 14:33:10 +0300

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Milliseconds.js
+
 A Node.js command line tool for reading data from nginx access logs
+
+## Usage
+
+```
+$ ./milliseconds.js --help
+
+  Usage: milliseconds [options]
+
+  Options:
+
+    -h, --help     output usage information
+    -V, --version  output the version number
+    -s, --start    Start time - Default: start of this month
+    -e, --end      End time - Default: now
+    --format       Output format <list,json> - Default: list
+
+```
--- milliseconds.js
+++ milliseconds.js
@@ -4,15 +4,10 @@ var commander = require('commander');
  
 commander
   .version('0.0.1')
-  .option('-p, --peppers', 'Add peppers')
-  .option('-P, --pineapple', 'Add pineapple')
-  .option('-b, --bbq-sauce', 'Add bbq sauce')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .option('-s, --start', 'Start time - Default: start of this month')
+  .option('-e, --end', 'End time - Default: now')
+  .option('--format', 'Output format <list,json> - Default: list')
   .parse(process.argv);
  
-console.log('you ordered a pizza with:');
-if (commander.peppers) console.log('  - peppers');
-if (commander.pineapple) console.log('  - pineapple');
-if (commander.bbqSauce) console.log('  - bbq');
-console.log('  - %s cheese', commander.cheese);
+console.log(commander);
 
